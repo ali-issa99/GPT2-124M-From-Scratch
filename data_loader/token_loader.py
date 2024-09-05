928f394f2ed6b4b3ab25e4de99148031e1038e21
Ali <a.issa@aems.ai>
2024-09-05 12:46:42 +0300

    code refactoring

---
+++ data_loader/token_loader.py
@@ -0,0 +1,9 @@
+import torch
+import numpy as np
+
+
+def load_tokens(filename):
+    npt = np.load(filename)
+    npt = npt.astype(np.int32)  # added after video
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
